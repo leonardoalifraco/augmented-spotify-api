a9b1b776c6077ba3c332b3b8fefe6488d66e4a13
Leonardo Alifraco <leonardo.alifraco@gmail.com>
2019-03-19 14:59:41 -0300

    Change hooks for serverless rack plugin and sinatra

---
+++ app/api.rb
@@ -0,0 +1,15 @@
+require "sinatra/base"
+
+require "./app/lib/spotify"
+
+class AugmentedSpotifyApi < Sinatra::Base
+  before do
+    content_type :json
+  end
+
+  get '/artists/:id' do
+    artist_id = params[:id]
+    artist_response = Spotify.new.find_artist(artist_id)
+    artist_response.body
+  end
+end
---
+++ app/lib/spotify.rb
@@ -0,0 +1,49 @@
+require "httparty"
+
+class Spotify
+  include HTTParty
+  base_uri 'https://api.spotify.com/v1/'
+  default_timeout 2
+
+  def find_artist(id)
+    options = { headers: headers }
+    self.class.get("/artists/#{id}", options)
+  end
+
+  private
+  def headers
+    { 'Authorization' => "#{token['token_type']} #{token['access_token']}" }
+  end
+
+  def token
+    return @token if valid_token?
+    @token = begin
+      new_token = request_token
+      new_token['expires_at'] = Time.now + new_token['expires_in']
+      new_token
+    end
+  end
+  
+  def valid_token?
+    defined?(@token) && (Time.now - 30) < @token['expires_at']
+  end
+
+  def request_token
+    client_id = ENV['SPOTIFY_CLIENT_ID']
+    client_secret = ENV['SPOTIFY_CLIENT_SECRET']
+    client_id_secret = Base64.strict_encode64("#{client_id}:#{client_secret}")
+    headers = { 'Authorization' => "Basic #{client_id_secret}" }
+    
+    body = { 'grant_type' => 'client_credentials' }
+    
+    response = HTTParty.post(
+      'https://accounts.spotify.com/api/token',
+      headers: headers,
+      body: body
+    )
+
+    if response.ok?
+      @token = response.parsed_response
+    end
+  end
+end
