0cb24fae517f3bc822c3003b062be87a80ca43a9
Leonardo Alifraco <leonardo.alifraco@gmail.com>
2019-03-25 14:08:11 -0300

    Remove handler template code

--- handler.rb
+++
@@ -1,7 +0,0 @@
-require 'json'
-require 'httparty'
-
-def hello(event:, context:)
-  response = HTTParty.get('http://worldclockapi.com/api/json/utc/now')
-  { statusCode: response.code, body: JSON.parse(response.body) }
-end
